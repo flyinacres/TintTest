d3b45a0d5b1df64417b1d005fb5669532e9a9070
Ronald Fischer <rff@ronalds-mbp-2.attlocal.net>
2015-10-25 00:50:14 -0700

    HSB change now working--nice effects for images!

--- TintTest/ViewController.swift
+++ TintTest/ViewController.swift
@@ -16,20 +16,53 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        let hsv = RGBtoHSV(1.0, g: 1.0, b: 0.0)
+        changeToHue(hsv.h, saturation: hsv.s, brightness: hsv.v)
+       
+    }
+    
+    // Hmm, changes the entire image to a flat color (if no matte used)
+    func tintImage() {
         let originalImage =  UIImage(named: "IMG_0300.JPG")
-//        let tintedImage = originalImage?.imageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate)
+        //        let tintedImage = originalImage?.imageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate)
         let tintedImage = originalImage?.imageWithRenderingMode(UIImageRenderingMode.Automatic)
         
         let imageView = UIImageView(image: tintedImage)
         imageView.tintColor = UIColor.blueColor()
         
         willImage.image = imageView.image
-        
-
-        
-        
+    }
+    
+    func RGBtoHSV(r : CGFloat, g : CGFloat, b : CGFloat) -> (h : CGFloat, s : CGFloat, v : CGFloat) {
+        var h : CGFloat = 0.0
+        var s : CGFloat = 0.0
+        var v : CGFloat = 0.0
+        let col = UIColor(red: r, green: g, blue: b, alpha: 1.0)
+        col.getHue(&h, saturation: &s, brightness: &v, alpha: nil)
+        return (h, s, v)
     }
 
+    // Nice--changes the image to a different hue.  Just what I wanted
+    func changeToHue(hue: CGFloat, saturation: CGFloat, brightness: CGFloat) {
+        var imageSize = CGSize(width: willImage.bounds.width, height: willImage.bounds.height)
+        UIGraphicsBeginImageContext(imageSize)
+        
+        let context = UIGraphicsGetCurrentContext()
+
+        let hueBlend = UIView(frame: willImage.bounds)
+        hueBlend.backgroundColor = UIColor(hue: CGFloat(hue), saturation: CGFloat(saturation), brightness: brightness, alpha: 1)
+            
+        
+        CGContextDrawImage(context, willImage.bounds, willImage.image?.CGImage);
+        CGContextSetBlendMode(context, kCGBlendModeHue);
+        
+        hueBlend.layer.renderInContext(context)
+        
+        willImage.image = UIGraphicsGetImageFromCurrentImageContext();
+        
+        UIGraphicsEndImageContext();
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
